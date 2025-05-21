9361200cc398db1e1519681f32dcae73afce4a8b
LuzIbarra7 <luzmabel.ibarraet12de1@gmail.com>
2025-05-21 18:22:45 -0300

    hola
    hola

--- TP_Supermercado/Consola/Supermercado.cs
+++ TP_Supermercado/Consola/Supermercado.cs
@@ -2,26 +2,45 @@ namespace TP_Supermercado;
 
 public class Supermercado
 {
-    Dictionary<string, string> productos = new Dictionary<string, string>(){
+    public List<Producto> productos = new List<Producto>();
 
 
-
-    };
-
-    void alta()
+    void alta(string Nombre, decimal PrecioUnitario, int Cantidad)
     {
-        
+        Producto nuevo = new Producto(Nombre, PrecioUnitario, Cantidad);
+
+        productos.Add(nuevo);
     }
 
-    void baja(){
-
-    }
-
-    void modificacion()
+    void baja(string Nombre)
     {
+        foreach (Producto p in productos)
+        {
+            if (p.Nombre == Nombre)
+            {
+                productos.Remove(p);
+                Console.WriteLine($"Producto '{Nombre}' eliminado correctamente.");
+                return;
+            }
+        }
 
+        Console.WriteLine($"Error: El producto '{Nombre}' no existe.");
     }
 
+    void modificacion(string Nombre, decimal nuevoPrecio, int cantidad)
+    {
+        foreach (Producto p in productos)
+        {
+            if (p.Nombre == Nombre)
+            {
+                p.PrecioUnitario = nuevoPrecio;
+                p.Cantidad += cantidad;
+                Console.WriteLine($"Producto '{Nombre}' modificado correctamente.");
+                return;
+            }
+        }
 
-    
+        Console.WriteLine($"Error: El producto '{Nombre}' no existe.");
+    }
+
 }
